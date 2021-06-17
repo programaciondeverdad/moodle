70694d7502970297bdbd2e6746e3d8cdf8ad454f
Mariana Casella <programaciondeverdad@gmail.com>
2021-06-17 19:56:00 -0300

    show errors

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
