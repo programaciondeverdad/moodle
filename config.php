85feb610909c81810e3beee32b58ec2c9f134a2f
Mariana Casella <programaciondeverdad@gmail.com>
2021-06-18 09:46:53 -0300

    config.php

--- config.php
+++ config.php
@@ -4,22 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-var_dump(getenv('pgsql'));
-$CFG->dbtype    = getenv('pgsql');
+$CFG->dbtype    = 'pgsql';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('ec2-52-4-111-46.compute-1.amazonaws.com');
-$CFG->dbname    = getenv('df3mkt527m08l5');
-$CFG->dbuser    = getenv('cdiluxcefqbjwz');
-$CFG->dbpass    = getenv('6b3aca855f7ef43f7be989164bcaae94565ba56447c1ed29306b6c5c6424a286');
+$CFG->dbhost    = 'ec2-52-4-111-46.compute-1.amazonaws.com';
+$CFG->dbname    = 'df3mkt527m08l5';
+$CFG->dbuser    = 'cdiluxcefqbjwz';
+$CFG->dbpass    = '6b3aca855f7ef43f7be989164bcaae94565ba56447c1ed29306b6c5c6424a286';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('5432'),
+  'dbport' => '5432',
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('https://sitioweb-programaciondeverdad.herokuapp.com');
-$CFG->dataroot  = getenv('/tmp');
+$CFG->wwwroot   = 'https://sitioweb-programaciondeverdad.herokuapp.com';
+$CFG->dataroot  = '/tmp';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 02777;
